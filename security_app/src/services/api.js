63b937e7dc15e3d7a3627d6cb8db3476715771c9
Achifa-dev <achifa.io.llc@gmail.com>
2024-09-28 21:16:15 +0100

    hl

--- security_app/src/services/api.js
+++ security_app/src/services/api.js
@@ -18,60 +18,60 @@ async function refreshAccessToken() {
   return accessToken;
 }
 
-api.interceptors.request.use(async (config) => {
-  if (openPaths.some(path => config.url.includes(path))) {
-    return config; // Bypass the interceptor logic
-  }
+// api.interceptors.request.use(async (config) => {
+//   if (openPaths.some(path => config.url.includes(path))) {
+//     return config; // Bypass the interceptor logic
+//   }
 
-  let token = window.localStorage.getItem('accessToken');
-  console.log(token)
-  if (token) {
-    let decodedToken = jwtDecode(token);
-    let currentTime = Date.now() / 1000;
+//   let token = window.localStorage.getItem('accessToken');
+//   console.log(token)
+//   if (token) {
+//     let decodedToken = jwtDecode(token);
+//     let currentTime = Date.now() / 1000;
 
-    if (decodedToken.exp < currentTime) {
-      console.log('Token expired, refreshing...');
-      try {
-        token = await refreshAccessToken();
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-  } else {
-    // If no token, log out the user
-    window.location.href = '/login';
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+//     if (decodedToken.exp < currentTime) {
+//       console.log('Token expired, refreshing...');
+//       try {
+//         token = await refreshAccessToken();
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     } else {
+//       config.headers['Authorization'] = `Bearer ${token}`;
+//     }
+//   } else {
+//     // If no token, log out the user
+//     window.location.href = '/login';
+//   }
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
 
 
-api.interceptors.response.use((response) => {
-  return response;
-}, async (error) => {
-  let originalRequest = error.config;
-  console.log(error.response.data, error.response)
-  if (error.response.status === 401 && !originalRequest._retry) {
-    originalRequest._retry = true;
-    if (error.response.data.message === 'Token expired' || error.response.data.message === 'Invalid token') {
-      try {
-        let accessToken = await refreshAccessToken();
-        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-        return api(originalRequest);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  } else if (error.response.status === 422) {
-    window.location.href = '/login'
-    window.localStorage.removeItem('accessToken');
-    window.localStorage.removeItem('refreshToken');
-  }
-  return Promise.reject(error);
-});
+// api.interceptors.response.use((response) => {
+//   return response;
+// }, async (error) => {
+//   let originalRequest = error.config;
+//   console.log(error.response.data, error.response)
+//   if (error.response.status === 401 && !originalRequest._retry) {
+//     originalRequest._retry = true;
+//     if (error.response.data.message === 'Token expired' || error.response.data.message === 'Invalid token') {
+//       try {
+//         let accessToken = await refreshAccessToken();
+//         axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+//         return api(originalRequest);
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//   } else if (error.response.status === 422) {
+//     window.location.href = '/login'
+//     window.localStorage.removeItem('accessToken');
+//     window.localStorage.removeItem('refreshToken');
+//   }
+//   return Promise.reject(error);
+// });
 
 
 export default api;
